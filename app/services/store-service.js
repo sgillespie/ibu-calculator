31c7fe53356faf8177ea920a305ed1b09af7bb00
Sean Gillespie <sean@mistersg.net>
2016-03-20 17:17:44 -0400

    Fix build failure

--- app/services/store-service.js
+++ app/services/store-service.js
@@ -1,23 +1,23 @@
-export default () => {
-  return {
-    targets: {
-      gravity: 1.048,
-      volume: 6.5,
-    },
+const defaultState = {
+  targets: {
+    gravity: 1.048,
+    volume: 6.5,
+  },
 
-    hops: [
-      {
-        id: 0,
-        type: 'Cascade',
-        weight: 1,
-        boilTime: 60,
-      },
-      {
-        id: 1,
-        type: 'Citra',
-        weight: 1.5,
-        boilTime: 10,
-      },
-    ],
-  };
+  hops: [
+    {
+      id: 0,
+      type: 'Cascade',
+      weight: 1,
+      boilTime: 60,
+    },
+    {
+      id: 1,
+      type: 'Citra',
+      weight: 1.5,
+      boilTime: 10,
+    },
+  ],
 };
+
+export default () => defaultState;
